b400c6a2c14b7b8d344af03f8aa32c190b819c95
James Dennis <jd@j2labs.net>
2012-04-02 00:18:38 -0400

    Removed unnecessary UserProfile model

--- models.py
+++ models.py
@@ -19,33 +19,6 @@ from brubeck.timekeeping import MillisecondField
 ### Adjust ID field in Brubeck models
 User = swap_field(User, ObjectIdField, ['id'])
 
-class UserProfile(Document):
-    """A UserProfile is essentially any publicly available info about the user.
-    Stored in a document separate from the User itself for security.
-    """
-    # ownable
-    owner = ObjectIdField(required=True)
-    username = StringField(max_length=30, required=True)
-
-    # streamable
-    created_at = MillisecondField()
-    updated_at = MillisecondField()
-
-    # unique fields
-    name = StringField(max_length=100)
-    location = StringField(max_length=100)
-    website = URLField()
-
-    class Meta:
-        id_field = ObjectIdField
-
-    _private_fields = [
-        'owner',
-    ]
-
-    def __unicode__(self):
-        return u'%s' % (self.username)
-
 
 ###
 ### List Models
